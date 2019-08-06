2e8872bec9ac0753ffa9bb0e8df7ed6b851601b3
Dongming Zhang <dzhang123@gmail.com>
2019-08-06 11:51:08 -0500

    genre controller and view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,3 +1,4 @@
+var validator = require('express-validator');
 var Genre = require('../models/genre');
 
 // Display list of all Genre.
@@ -12,13 +13,39 @@ exports.genre_detail = function (req, res) {
 
 // Display Genre create form on GET.
 exports.genre_create_get = function (req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+    res.render('genre_form', {title: 'Create Genre'});
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+exports.genre_create_post = [
+    validator.body('name', 'Genre name required').isLength({min: 1}).trim(),
+    validator.sanitizeBody('name').escape(),
+
+    (req, res, next) => {
+        const errors = validator.validationResult(req);
+        var genre = new Genre({name: req.body.name});
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages
+            res.render('genre_form', {title: 'Create Genre', genre: genre, errors: errors.array()});
+        }
+        else {
+            // Data from form is valid. check if Genre with the same name already exists.
+            Genre.findOne({'name': req.body.name})
+                .exec( (err, found_genre) => {
+                    if (err) {return next(err);}
+                    if (found_genre) {
+                        res.redirect(found_genre.url);
+                    } else {
+                        genre.save( (err) => {
+                            if (err) { return next(err);}
+                            res.redirect(genre.url);
+                        })
+                    }
+                });
+        }
+    }
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function (req, res) {
