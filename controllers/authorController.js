e8a46ab167eeec685fb77b17c056156d296fd881
Dongming Zhang <dzhang123@gmail.com>
2019-08-06 13:01:19 -0500

    author controller and form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,3 +1,6 @@
+// var validator = require('express-validator');
+var {body, validationResult} = require('express-validator/check');
+var {sanitizeBody} = require('express-validator/filter');
 var Author = require('../models/author');
 
 exports.author_list = (req, res) => {
@@ -9,12 +12,43 @@ exports.author_detail = (req, res) => {
 };
 
 exports.author_create_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author create GET');
+    res.render ('author_form', {title: 'Create Author'});
 };
 
-exports.author_create_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+    body('first_name').isLength({min: 1}).trim().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body.apply('family_name').isLength({min: 1}).trim().withMessage('Family name must be specified')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+
+    body('date_of_birth', 'Invalid date of birth').optional({checkFalsy: true}).isISO8601(),
+    body('date_of_death', 'Invalid date of death').optional({checkFalsy: true}).isISO8601(),
+
+    sanitizeBody('first_name').escape(),
+    sanitizeBody('family_name').escape(),
+    sanitizeBody('date_of_birth').toDate(),
+    sanitizeBody('date_of_death').toDate(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render('author_form', {title: 'Create Author', author: req.body, errors: errors.array()});
+            return;
+        } else {
+            var author = new Author({
+                first_name: req.body.first_name,
+                family_name: req.body.family_name,
+                date_of_birth: req.body.date_of_birth,
+                date_of_death: req.body.date_of_death
+            });
+            author.save ((err) => {
+                if (err) { return next(err);}
+                res.redirect(author.url);
+            })
+        }
+    }
+];
 
 exports.author_delete_get = (req, res) => {
     res.send('NOT IMPLEMENTED: Author delete GET');
