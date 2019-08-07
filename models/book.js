613694ac79f5e2e9ff56da82910869080e12e168
Dongming Zhang <dzhang123@gmail.com>
2019-08-06 23:04:28 -0500

    updated from source

--- models/book.js
+++ models/book.js
@@ -1,20 +1,21 @@
-
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var BookSchema = new Schema ({
+var BookSchema = new Schema({
     title: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, ref: 'Author', reuqired: true},
+    author: { type: Schema.ObjectId, ref: 'Author', required: true },
     summary: {type: String, required: true},
     isbn: {type: String, required: true},
-    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+    genre: [{ type: Schema.ObjectId, ref: 'Genre' }]
 });
 
+// Virtual for this book instance URL.
 BookSchema
 .virtual('url')
-.get( () => {
-    return `/catalog/book/${this._id}`;
+.get(function () {
+  return '/catalog/book/'+this._id;
 });
 
+// Export model.
 module.exports = mongoose.model('Book', BookSchema);
